7b4ff161b7c8ab51bdd482b9e1983505eaff8f07
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-03-25 16:08:19 +0100

    Run resque in a specific namespace during specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ RSpec.configure do |config|
 
   config.mock_with :rspec
 
+  config.before(:suite) do
+    Resque.redis.namespace = "resque:rspec"
+  end
+
   config.after(:each) do
     Resque.queues.each{|queue| Resque.remove_queue(queue) }
   end
