3c1cc6079b680ca274e4dce0ab41013ed08a58a2
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-04-04 19:29:16 +0200

    require 'ruote/resque/client' loads only the files needed on the workers

---
+++ lib/ruote/resque/client.rb
@@ -0,0 +1,36 @@
+require 'logger'
+require 'ruote/resque/job'
+require 'ruote/resque/reply_job'
+require 'ruote/resque/version'
+
+module Ruote
+  module Resque
+    class << self
+
+      attr_accessor :configuration
+
+      def logger
+        configuration.logger
+      end
+
+      def configure
+        self.configuration ||= Configuration.new
+        yield(configuration) if block_given?
+      end
+
+      class Configuration
+        attr_accessor :reply_queue
+        attr_accessor :logger
+        attr_accessor :interval
+      end
+
+    end
+  end
+end
+
+# setup default configuration
+Ruote::Resque.configure do |config|
+  config.reply_queue = :ruote_reply
+  config.logger = Logger.new(STDOUT).tap { |log| log.level = Logger::INFO }
+  config.interval = 5
+end
