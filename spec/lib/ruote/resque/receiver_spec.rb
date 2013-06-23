a820ec65a3e9fa4fcc38994bf4df777144620556
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-06-23 23:59:10 +0200

    Add specs for error handling in receiver

--- spec/lib/ruote/resque/receiver_spec.rb
+++ spec/lib/ruote/resque/receiver_spec.rb
@@ -28,6 +28,8 @@ describe Ruote::Resque::Receiver do
 
   before :each do
 
+    Resque.redis.flushdb
+
     @board = Ruote::Dashboard.new(Ruote::Worker.new(Ruote::HashStorage.new))
     # @board.noisy = true
 
@@ -70,6 +72,41 @@ describe Ruote::Resque::Receiver do
       resque_bravo
       block_charly
     end
+  end
+
+  context 'invalid job handling' do
+
+    it 'raises InvalidJob' do
+
+      class AnotherJob
+      end
+
+      Ruote::Resque::Receiver.any_instance.should_receive(:handle_error) do |e|
+        e.class.should eq(Ruote::Resque::InvalidJob)
+      end
+
+      Resque.enqueue_to(Ruote::Resque.configuration.reply_queue, 'AnotherJob')
+
+      # Ensure it is picked up by the receiver + cleanup afterwards
+      sleep 2
+      ::Resque.reserve(Ruote::Resque.configuration.reply_queue)
+
+    end
+
+    it 'raises InvalidWorkitem' do
+
+      Ruote::Resque::Receiver.any_instance.should_receive(:handle_error) do |e|
+        e.class.should eq(Ruote::Resque::InvalidWorkitem)
+      end
+
+      Resque.enqueue_to(Ruote::Resque.configuration.reply_queue, Ruote::Resque::ReplyJob, {})
+
+      # Ensure it is picked up by the receiver + cleanup afterwards
+      sleep 2
+      ::Resque.reserve(Ruote::Resque.configuration.reply_queue)
+
+    end
+
   end
 
   context 'participant/reply flow' do
@@ -117,6 +154,21 @@ describe Ruote::Resque::Receiver do
         r['workitem']['fields']['resque_bravo'].should eq(nil)
         r['workitem']['fields']['block_charly'].should eq(nil)
         r['workitem']['fields']['block_delta'].should eq('was here')
+      end
+
+      it 'marks the job as failed in Resque' do
+
+        wfid = @board.launch(definition)
+
+        r = @board.wait_for(wfid, :timeout => RUOTE_WAIT_TIMEOUT)
+          # wait until process terminates or hits an error
+
+        Resque::Failure.count.should eq(1)
+        failed = Resque::Failure.all(0, 1)
+        failed.class.should eq(Hash)
+        failed['payload']['class'].should eq('BravoFailureJob')
+        failed['exception'].should eq('BravoError')
+        failed['error'].should eq('im a failure')
       end
 
       it 'can be replayed from Ruote' do
